da5c4d0404e717daf071120081fb7c68b43df5c2
aracdo <40762424+aracdo@users.noreply.github.com>
2020-04-06 14:13:38 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+void printVector(vector <int> a) {
+	for (auto now : a) cout << now << " ";
+	cout << endl;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector <int> a(n);
+	for (int i = 0; i < n; i++) cin >> a[i];
+	sort(a.begin(), a.end());
+	printVector(a);
+	return 0;
+}
